69980b0fb7b20ef02baa1ad2cee1cbefbf3a80e8
robsalasco <wala.rsalas@gmail.com>
2017-02-25 14:33:25 -0300

    Translation and warning

--- 5_Participacion_politica/Brasilia.R
+++ 5_Participacion_politica/Brasilia.R
@@ -1,43 +1,45 @@
 library(photon)
 library(sp)
 
-# Leer base de datos del padron
+##############################-DISCLAIMER-#############################
+#                                                                     #
+#                                                                     # 
+#                  THIS FILE IS A PROOF OF CONCEPT                    #
+#                                                                     #
+#                                                                     #
+#######################################################################
 
-## Padron San Miguel
-
+# Reading voter registration list of San Miguel municipality
 A1321021 <- read_delim(file("Data/A1321021.csv",encoding="UTF-8"), ";", escape_double = FALSE, trim_ws = TRUE)
 
-# Subset de las calles involucradas en los barrios piloto
-
-capa_calle <- read_csv("Data/capa_calle.csv")
+# Subsetting
+capa_calle <- read_csv("Data/capa_calle.csv") # I'll work on a solution to make this file using R instead of QGIS
 capa_calle <- as.vector(sapply(capa_calle$name, toupper))
 calles <- unique(grep(paste(capa_calle,collapse="|"), A1321021$Domicilio, value=TRUE))
 subset_calles <- A1321021 %>% filter(Domicilio %in% calles)
 rm(list=c("capa_calle","calles"))
 
-# Geocoding de las personas que coinciden con la localizacion
+# Geocoding using photon
 loc <-  as.vector(sapply(subset_calles$Domicilio,function(x){paste(x,", SAN MIGUEL, CHILE",sep="")}))
 coord <- geocode(loc, limit = 1)
 write.csv(coord,"Data/Brasilia_Geocoding.csv")
 rm(loc)
 
-## ¿Cuanto acierto del geocoding? ~ 75.5% GOOD
+# Geocoding match rate ~ 75.5% GOOD
 coord %>% summarise(total=n(),countNA=sum(is.na(lon)),per=(sum(!is.na(lon))/n())*100)
 
-# Merge de geocoding con la base original
+# Merging geocoding results with original database
 subset_calles$location <-  as.vector(sapply(subset_calles$Domicilio,function(x){paste(x,", SAN MIGUEL, CHILE",sep="")}))
 coordenadas <- coord %>% select(location,lat,lon)
 dat <- merge(subset_calles,coordenadas,by="location")
 rm(list=c("coordenadas","subset_calles"))
 
-# Hacer coincidir barrios con las personas del padron
-
+# Matching individuals with voter registration list
 crs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
 
 dat <- dat[!is.na(dat$lon),]
 coordinates(dat) <- ~lon+lat
 proj4string(dat) <- crs
-dat <- spTransform(dat, crs)
 
 shape_pil <- readOGR(dsn = "../Censo2012/Shapes/barrios_piloto/", layer = "barrios_piloto")
 shape_pil <- spTransform(shape_pil, crs)
@@ -45,8 +47,7 @@ brasilia <- shape_pil %>% filter(BARRIO == "Brasilia")
 
 final <- dat[brasilia,]
 
-# Ver las mesas que estan involucradas haciendo un summarize count
-
+# Counting "mesas" using summarize count
 count <- as.data.frame(final[!duplicated(final$RUT), ]) %>%  
   group_by(Mesa) %>% summarise(n=n()) %>% 
   arrange(desc(n))
@@ -57,19 +58,11 @@ total_mesas <- as.data.frame(A1321021[!duplicated(A1321021$RUT), ]) %>%
 
 count$total <- total_mesas$n
 
-
-# Usando las bases del servel ver cuantos votos hay por mesa es decir cuantas personas participaron en la eleccion 
-# https://www.servel.cl/participacion-electoral/
-
+# Using election database we need to count how many votes we have per "mesa" to have the participation rate
 elecciones <- read_excel("Data/13_Resultados_Mesa_Alcaldes_Ter.xlsx",sheet=1,col_types=c("numeric","text","numeric","numeric","text","text","numeric","text","numeric","text","text","text","numeric"))
-
-# Obtener % participacion por mesa
-
 votospormesa <- elecciones %>% filter(Comuna=="SAN MIGUEL") %>% unite(Mesa, c(`Nro. Mesa`,`Tipo Mesa`),sep=" ") %>% group_by(Mesa) %>% summarise(votos=sum(`Votos TER`))
 votos <-  merge(count,votospormesa,by="Mesa")
-
 votos <- votos %>% mutate(participacion=votos/total) 
 
-# Media de participacion en las mesas donde los vecinos del barrio votan
+# This isn't the best way to do it but is something. I'll work on this later.
 mean(votos$participacion)
-
