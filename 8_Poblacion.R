1262d4d9a2a02c88871f4d340f45ac4478f9ec70
Roberto Salas <wala.rsalas@gmail.com>
2017-11-08 12:31:32 -0300

    Adding new indicator (it was calculated before but not pushed)

---
+++ 8_Poblacion.R
@@ -0,0 +1,31 @@
+# Poblacion total
+
+#Connections 
+library(RMySQL)
+library(tidyverse)
+
+con <- src_mysql(dbname = "censo2012",
+                 host = Sys.getenv("MYSQL_HOST"),
+                 user = Sys.getenv("MYSQL_USER"),
+                 password = Sys.getenv("MYSQL_PASS")
+                 )
+
+#Start querying
+
+# Generating ID_W
+geoid <- tbl(con,"idgeo") %>% 
+  select(FOLIO,NVIV,COM,DTO,AREA,ZONA,MZ,LOCALIDAD,ENTIDAD,SECTOR) %>% 
+  collect(n=Inf) %>% 
+  mutate(ID_W=paste(COM,sprintf("%02d", DTO),AREA,ifelse(AREA==1, sprintf("%03d",ZONA), ifelse(AREA==2, sprintf("%03d",LOCALIDAD), 0)),ifelse(AREA==1, sprintf("%03d",MZ), ifelse(AREA==2, sprintf("%03d",ENTIDAD), 0)),sep="")) %>% 
+  select(FOLIO,NVIV,ID_W)
+
+# Calculating population by manzana
+
+numberpeople <-tbl(con,"poblacion") %>%
+  select(FOLIO,NVIV) %>% collect(n=Inf) %>% 
+  left_join(geoid, by=c("FOLIO"="FOLIO","NVIV"="NVIV")) %>% 
+  group_by(ID_W) %>% 
+  summarize(listo = n())
+
+  writeRDS(numberpeople,"poblacion.RDS")
+
